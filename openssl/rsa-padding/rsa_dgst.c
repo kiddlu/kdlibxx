1cf9fb02e7c5fdbf863462f0ca005c0105c9a3a1
kiddlu <kidd.dawny.lu@gmail.com>
2019-04-11 10:33:26 +0000

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- openssl/rsa-padding/rsa_dgst.c
+++ openssl/rsa-padding/rsa_dgst.c
@@ -1,5 +1,8 @@
 //apt install libssl-dev
 #include <string.h>
+#include <inttypes.h>
+#include <sys/time.h>
+
 #include <openssl/rsa.h>
 #include <openssl/pem.h>
 #include <openssl/err.h>
@@ -8,38 +11,6 @@
 
 #include "file.h"
 
-#include <inttypes.h>
-#include <sys/time.h>
-
-EVP_PKEY *get_private_key(char* path)
-{
-    FILE    *fp = NULL; 
-    char    key_path[1024];
-    EVP_PKEY   *priv_key = NULL;
-
-    memset(key_path, 0 ,sizeof(key_path));
-
-    if(256 < strlen(path))
-        strncpy(key_path, path, 256);
-    else
-        strncpy(key_path, path, strlen(path));
-
-    if(NULL == (fp = fopen(key_path, "rb")))
-    {
-        printf( "open key error[%s]\n", key_path);
-        return NULL;
-    }
-
-    if(NULL == (priv_key = PEM_read_PrivateKey(fp, NULL, NULL,NULL)))
-    {
-        printf( "read key error\n");
-        fclose(fp);
-        return NULL;
-    }
-    fclose(fp);
-
-    return priv_key;
-}
 
 #define MAX_SIGNATURE_LEN 2048
 
@@ -47,7 +18,8 @@ int main(void)
 {
     int     ret;
     char    signature[MAX_SIGNATURE_LEN];
-    size_t  sig_len;
+    size_t  sig_len = MAX_SIGNATURE_LEN;
+    FILE    *fp;
 
     int     saltlen = 32;
 
@@ -55,22 +27,25 @@ int main(void)
     EVP_MD_CTX *md_ctx;
     EVP_PKEY_CTX *pkey_ctx;
 
-    priv_key = get_private_key("rsa_priv.pem");
+    fp =  fopen("rsa_priv.pem", "rb");
+    priv_key = PEM_read_PrivateKey(fp, NULL, NULL, NULL);
+    fclose(fp);
 
     memset(signature, 0, sizeof(signature));
 
     md_ctx = EVP_MD_CTX_create();
+
     EVP_DigestSignInit(md_ctx, &pkey_ctx, EVP_sha256(), NULL, priv_key);
+
     EVP_PKEY_CTX_set_rsa_padding(pkey_ctx, RSA_PKCS1_PSS_PADDING);
+
     EVP_PKEY_CTX_set_rsa_pss_saltlen(pkey_ctx, saltlen);
 
     EVP_DigestSignUpdate(md_ctx, file_buf, file_len);
 
     EVP_DigestSignFinal(md_ctx, signature, &sig_len);
 
-    printf("%d\n", sig_len);
-
-    FILE *fp = fopen("file.sig","wb");
+    fp = fopen("file.sig","wb");
     fwrite(signature, sig_len, 1, fp);
     fclose(fp);
 
